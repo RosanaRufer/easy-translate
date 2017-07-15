35745270ba95546ca283388c2d0b577d036e3d6f
rosana ruiz <rosanarufer@gmail.com>
2017-07-15 12:52:10 +0100

    Added dummy unit tests

---
+++ src/reducers/translation.reducer.js
@@ -0,0 +1,13 @@
+export const translationReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_MESSAGE_VALUE':
+      return state.languageCardSet[action.langIndex].messages[action.key] = action.value;
+    default:
+      return state;
+  }
+};
+
+function setMessageValue(state, langIndex, key, value){
+    let newState = {...state};
+    return newState;
+}
---
+++ src/reducers/translation.reducer.spec.js
@@ -0,0 +1,19 @@
+import {translationReducer} from './translation.reducer'
+
+describe("translationReducer", function() {
+  it("should be a function", function() {
+    //Assert
+    expect(typeof translationReducer).toBe('function');
+  });
+
+  it('should return initial state when passing initial state as undefined', () => {
+      
+  });  
+});
+
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+});
+
